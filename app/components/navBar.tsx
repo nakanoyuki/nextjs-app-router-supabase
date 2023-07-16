7779b36ab9967bb9309706d717178ebcde97ba62
nakanoyuki <yukich41@gmail.com>
2023-07-16 22:31:26 +0900

    Blogのデータを取得

--- app/components/navBar.tsx
+++ app/components/navBar.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 
-export default function navBar() {
+export default function NavBar() {
   return (
     <header className="bg-gray-800 p-4">
       <nav className="space-x-4">
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import Image from 'next/image'
+import NoteList from './components/noteList'
 
 export default function Home() {
   return (
-<main>
-  <div className="m-10 text-center">Hello World</div>
-</main>
+    <main>
+      <div className="m-10 text-center">
+        <p>Hello World</p>
+        <NoteList />
+      </div>
+    </main>
   )
 }
